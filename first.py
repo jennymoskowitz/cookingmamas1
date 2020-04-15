8f48a1992d057163fca76df14b737fc6313f6282
jessicanabi <jessnabi@umich.edu>
2020-04-15 16:27:37 -0400

    started  get dict for spoon

--- first.py
+++ first.py
@@ -36,130 +36,40 @@ def write_cache(CACHE_FNAME, CACHE_DICT):
         json.dump(CACHE_DICT, f)
 
 
+#Spoonacular 
+#get recipies
 class Spoonacular:
- 
-    # def __init__(self, api_key, timeout=5, sleep_time=1.5, allow_extra_calls=False):
-    #     """ Spoonacular API Constructor
-    #     :param api_key: key provided by Spoonacular (str)
-    #     :param timeout: time before quitting on response (seconds)
-    #     :param sleep_time: time to wait between requests (seconds)
-    #     :param allow_extra_calls: override the API call limit (bool)
-    #     """
-    #     assert api_key != 'd6138d2114b64120b05c0cbdb7c92f60'
-    #     self.api_key = api_key
-    #     self.api_root = "https://api.spoonacular.com/"
-    #     self.timeout = timeout
-    #     self.sleep_time = max(sleep_time, 1)  # Rate limiting TODO: Make this immutable
-    #     self.allow_extra_calls = allow_extra_calls
 
-    # def _make_request(self, path, method='GET', endpoint=None, query_=None, params_=None, json_=None):
-    #     """ Make a request to the API """
-
-    #     # Check if the API call cost will exceed the quota
-    #     endpoint = inspect.stack()[1].function
-    #     try:
-    #         uri = self.api_root + path
-
-    #         # API auth (temporary kludge)
-    #         if params_:
-    #             params_['apiKey'] = self.api_key
-    #         else:
-    #             params_ = {'apiKey': self.api_key}
-    #         response = self.session.request(method, uri, timeout=self.timeout, data=query_, params=params_, json=json_)
-    #     except socket.timeout as e:
-    #         print("Timeout raised and caught: {}".format(e))
-    #         return
-    #     time.sleep(self.sleep_time)  # Enforce rate limiting
-    #     return response
-
-    #def _make_request(self, method='GET', params_=None, json_=None):
-    # """ Make a request to the API """
-    # uri = "https://api.spoonacular.com/"
-
-        #params_ = {'apiKey': 'd6138d2114b64120b05c0cbdb7c92f60'}
-        #response = self.session.request(method, uri,
-        #                              timeout=self.timeout,
-        #                              data=query_,
-        #                              params=params_,
-        #                             json=json_)
-        #return response
-
-
-
-
-    # def get_random_recipes(self, limitLicense=None, number=100, tags=None):
-    #     """ Find random (popular) recipes.
-    #     https://spoonacular.com/food-api/docs#get-random-recipes
-    #     """
-    #     endpoint = "recipes/random"
-    #     url_query = {}
-    #     url_params = {"limitLicense": limitLicense, "number": number, "tags": tags}
-    #     return self._make_request(endpoint, method ='GET', query_=url_query, params_=url_params)
-    
     def get_recipies(self):
         url = 'https://api.spoonacular.com/recipes/random'
-        params = {"apiKey" : '3adec4cbad224f2c9596d4c011d346fc', "number" : "100"}
+        params = {"apiKey" : '3adec4cbad224f2c9596d4c011d346fc', "number" : "1"}
         response = requests.request("GET", url, params = params)
         print(response.text)
         #data = json.loads(r.text)
-        return response.text
+        return response
 
-        #https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert
-# var1 = Spoonacular()
-# var1.get_recipies()
-
-
-
-
-
-
-
-# class Tasty:
-#     def get_tasty_recipies(self):
-#         url = "https://tasty.p.rapidapi.com/recipes/list"
-
-#         querystring = {"from":"0","sizes":"100"}
-
-#         headers = {'x-rapidapi-host': "tasty.p.rapidapi.com",'x-rapidapi-key': "74c1de20bdmsh109b356a35082c3p1cf14cjsn37f52eca5a61"}
-
-#         response = requests.request("GET", url, headers=headers, params=querystring)
-
-#         print(response.text)
-#         return response.text
-
-# var = Tasty()
-# var.get_tasty_recipies()
+    def get_dict(self):
+        dict1 = {}
+        r = self.get_recipies()
+        data = json.loads(r.text)
+        print(data)
+        for x in range(len(data['recipes'])):
+            try:
+                if len(data['recipes'][x]['cuisines']) > 0:
+                    if data['recipes'][x]['cuisines'][0] not in dict1:
+                        dict1[data['recipes'][x]['cuisines'][0]] = 0
+                    dict1[data['recipes'][x]['cuisines'][0]] += 1
+                else:
+                    print("Cuisine not found.")
 
 
-# cuisinelist = ['american', 'italian', 'asian', 'mexican', 'southern', 'french', 'southwestern',
-#             'barbecue-bbq', 'indian', 'chinese', 'cajun', 'mediterranean', 'greek', 'english',
-#             'spanish', 'thai', 'german', 'moroccan', 'irish', 'japanese', 'cuban', 
-#             'hawaiian', 'swedish', 'hungarian', 'portuguese']
-# resultsPerCuisine = 500
+                    
 
-# create wrapper function
-# def recipeOutputter(cuis):
-#     scuisine = '&allowedCuisine[]=cuisine^cuisine-' + cuis
-#     sresults = '&maxResult=' + str(resultsPerCuisine)
-    
-#     # retrieve and store results in a DataFrame
-#     r = (requests.get('http://api.yummly.com/v1/api/recipes?' + cykey + scuisine + sresults)).json()
-#     recipes = pd.DataFrame(r['matches'], columns = ['id', 'recipeName', 'rating', 'totalTimeInSeconds', 'ingredients'])
-    
-#     # extract course and cusine and add to DF
-#     course = []
-#     cuisine = []
-#     for i in r['matches']:
-#         course.append(i['attributes'].get('course', None))
-#         cuisine.append(i['attributes'].get('cuisine', None))
-#     recipes['course'] = course
-#     recipes['cuisine'] = cuisine
-    
-#     # rearrange DF
-#     recipes.set_index('id', inplace=True)
-#     col = ['recipeName', 'rating', 'totalTimeInSeconds', 'course', 'cuisine', 'ingredients']
-#     recipes=recipes[col]
-#     https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert
+
+    #   
+
+
+
 
 class Tasty:
     def get_tasty_recipes(self):
@@ -250,6 +160,8 @@ class Tasty:
         plt.bar(names, values)
         plt.suptitle("Top Cuisines")
         plt.show()
+
+
 
 
 var = Tasty()
